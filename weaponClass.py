d4217d391da0f40b9a4db024442ceadb30c2a8cc
Lyndsey <lyndsey_davis2@baylor.edu>
2022-02-16 14:11:08 -0600

    weapon update1

--- weaponClass.py
+++ weaponClass.py
@@ -22,6 +22,46 @@ every time the method is called. When the bullet count reaches zero, it should c
 the attribute 'status' to 'Inactive'
 
 '''
+class Weapon:
+
+    def __init__(self, name, speed, range):
+        self.__name = name
+        self.__bullets = 0
+        self.__speed = speed
+        self.__range = range
+        self.__status = "Active"
+    
+    def set_name(self, name):
+        self.__name = str(name)
+
+    def set_speed(self, speed):
+        self.__speed = speed
+    
+    def set_bullets(self):
+        self.__bullets = random.randint(10, 100000)
+    
+    def set_range(self, range):
+        self.__range = range
+    
+    def fire_bullet(self):
+        for i in range(self.__bullets):
+            if self.__bullets == 0:
+                self.__bullets -= 1
+                self.__status = "Active"
+            else: 
+                self.__status = "Inactive"
+    
+    def get_bullets(self):
+        return self.__bullets
+    def get_status(self):
+        return self.__status
+    def get_name(self):
+        return self.__name
+    def get_range(self):
+        return self.__range 
+    def get_speed(self):
+        return self.__speed           
+
 
 
 
--- weaponProgram.py
+++ weaponProgram.py
@@ -15,44 +15,41 @@ HINT: Follow the comments for each line to help with the logic of the problem.
 
 
 # create a file object to open the file in read mode
-
-
-
+weapons_in = open("weapons.txt", "r")
+weapons_read = weapons_in.read()
+weapons_in.close()
 # create a csv object from the file object
 
-
+outfile = open("fire_weapons.csv", "w")
 
 #skip the header row
-
-
-
+next(weapons_read)
 
 #create an empty dictionary named 'weapons_dict'
-
-
-
+weapons_dict = {}
 
 #use a for loop to iterate through every row of the csv file
+for record in weapons_read:
 
     #use variables for name,speed and range (optional)
     
 
     # create an instance of the weapon object using the 
     # specs from the csv file (name,speed and range) 
-    
+    outfile.write(record[1]+ record[2]+record[3]"\n")
 
     # append the name and bullet count to 'weapons_dict'
     
 
 
     # print out the name of the weapon using the appropriate method of the object 
-    
+    print("Weapon Name: ",)
     # print out the speed of the weapon using the appropriate method of the object
-    
+    print("Weapon Speed: ")
     # print out the range of the weapon using the appropriate method of the object
-    
+    print("Weapon Range: ")
     # print out the number of bullets of the weapon using the appropriate method of the object
-    
+    print("Number of Bullets",)
 
     #use an input statement to halt the program and wait for the user - 
     input("Press any key to fire the weapon")
